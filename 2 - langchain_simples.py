d0cab42bd644868ddd95cc523230aec165ccfd42
josue-borsoni <josue.borsoni@gmail.com>
2024-10-08 15:56:58 -0300

    Update 2 - langchain_simples.py

--- 2 - langchain_simples.py
+++ 2 - langchain_simples.py
@@ -9,6 +9,7 @@ load_dotenv() # Carrega o arquivo .env com a chave da api
 #prompt = f"Escreva um resumo sobre a linguagem de programação {linguagem}"
 
 #RMD-901 
+#RDM-902
 
 # Modelo de Prompt Template
 modelo_do_prompt = PromptTemplate.from_template ("Escreva um resumo sobre a linguagem {lng} e um método para calcular o {clc}")
